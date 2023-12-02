257ca6393cdfc9f0d1a8b750044692d76a0cef23
Augusto Quisquinay <augustoacual@gmail.com>
2023-12-01 22:02:00 -0600

    Form Controller

---
+++ templates/form.html.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Enter your name</title>
+</head>
+
+<body>
+  <form action="" method="post">
+    <label for="firstname">First name:</label>
+    <input type="text" name="firstname" id="firstname" />
+
+    <label for="lastname">Last name:</label>
+    <input type="text" name="lastname" id="lastname" />
+
+    <input type="submit" value="GO" />
+  </form>
+</body>
+
+</html>
