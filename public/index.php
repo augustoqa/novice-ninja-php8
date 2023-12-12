23e0e4f6e00d384142d698bf020bbaf9fd9055ae
Augusto Quisquinay <augustoacual@gmail.com>
2023-12-12 16:14:23 -0600

    Create table in database

--- public/index.php
+++ public/index.php
@@ -1,9 +1,23 @@
 <?php
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'admin');
-    $output = 'Database connection established.';
+
+    $sql = 'CREATE TABLE joke (
+        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        joketext TEXT,
+        jokedate DATE NOT NULL
+    ) DEFAULT CHARACTER SET utf8 ENGINE=InnoDB';
+
+    $pdo->exec($sql);
+
+    $output = 'Joke table successfully created.';
 } catch (PDOException $e) {
-    $output = 'Unable to connect to the database server: ' . $e->getMessage();
+    $output = sprintf(
+        'Database error: %s in %s:%s',
+        $e->getMessage(),
+        $e->getFile(),
+        $e->getLine()
+    );
 }
 
 include __DIR__ . '/../templates/output.html.php';
