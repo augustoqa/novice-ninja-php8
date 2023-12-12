c02db97104d0a6b999f21d7342bf4dca2e6857ce
Augusto Quisquinay <augustoacual@gmail.com>
2023-12-12 15:53:16 -0600

    Fix username database

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8mb4', 'ijdbuse', 'admin');
+    $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'admin');
     $output = 'Database connection established.';
 } catch (PDOException $e) {
     $output = 'Unable to connect to the database server: ' . $e->getMessage();
