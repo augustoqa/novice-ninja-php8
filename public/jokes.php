d172dab7d664eead65cf0b14c2dcac0d6b5e1c69
Augusto Quisquinay <augustoacual@gmail.com>
2023-12-29 16:36:46 -0600

    Select jokes and its authors

--- public/jokes.php
+++ public/jokes.php
@@ -2,7 +2,9 @@
 try {
     $pdo = new PDO('mysql:host=localhost;dbname=ijdb;charset=utf8mb4', 'ijdbuser', 'admin');
 
-    $sql = 'SELECT `id`, `joketext` FROM `joke`';
+    $sql = 'SELECT `joke`.`id`, `joketext`, `name`, `email` 
+            FROM `joke` INNER JOIN `author`
+                ON `authorid` = `author`.`id`';
     $jokes = $pdo->query($sql);
 
     $title = 'Joke list';
