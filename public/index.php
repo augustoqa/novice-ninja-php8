8286ad709b494542aefccb1613438a2423af96d9
Cesar Quisquinay <augustoacual@gmail.com>
2024-05-16 17:49:19 -0600

    Handler controller dinamically

--- public/index.php
+++ public/index.php
@@ -15,19 +15,28 @@ try {
 	include __DIR__ . '/../includes/DatabaseConnection.php';
 	include __DIR__ . '/../classes/DatabaseTable.php';
 	include __DIR__ . '/../controllers/JokeController.php';
+	// include __DIR__ . '/../controllers/AuthorController.php';
 
 	$jokesTable   = new DatabaseTable($pdo, 'joke', 'id');
 	$authorsTable = new DatabaseTable($pdo, 'author', 'id');
 
-	$jokeController = new JokeController($jokesTable, $authorsTable);
-
 	$action = $_GET['action'] ?? 'home';
+	$controllerName = $_GET['controller'] ?? 'joke';
 
-	if ($action == strtolower($action)) {
-		$page = $jokeController->$action();
+	if ($controllerName === 'joke') {
+		$controller = new JokeController($jokesTable, $authorsTable);
+	} else if ($controllerName === 'author') {
+		$controller = new AuthorController($authorsTable);
+	}
+
+	if ($action == strtolower($action) && $controllerName == strtolower($controllerName)) {
+		$page = $controller->$action();
 	} else {
 		http_response_code(301);
-		header('location: index.php?action=' . strtolower($action));
+		header(sprintf('location: index.php?controller=%s&action=%s',
+			strtolower($controllerName),
+			strtolower($action)
+		));
 		exit;
 	}
 
