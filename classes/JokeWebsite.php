c64c0d431c727d8f70f5b34d3575659538117f37
Cesar Quisquinay <augustoacual@gmail.com>
2024-05-31 15:13:06 -0600

    Create Autoload for classes

--- classes/JokeWebsite.php
+++ classes/JokeWebsite.php
@@ -10,7 +10,6 @@ class JokeWebsite
 	public function getController(string $controllerName)
 	{
 		include __DIR__ . '/../includes/DatabaseConnection.php';
-		include __DIR__ . '/../classes/DatabaseTable.php';
 		include __DIR__ . '/../controllers/JokeController.php';
 		// include __DIR__ . '/../controllers/AuthorController.php';
 
