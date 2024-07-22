de301df6715ac2c01091c409d668896fa3f596b8
Cesar Quisquinay <augustoacual@gmail.com>
2024-07-22 12:11:47 -0600

    Session and Cookie

---
+++ public/sessions.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION['visits'])) {
+	$_SESSION['visits'] = 0;
+}
+$_SESSION['visits'] = $_SESSION['visits'] + 1;
+
+if ($_SESSION['visits'] > 1) {
+	echo 'This is visit ' . $_SESSION['visits'];
+} else {
+	// First visit
+	echo 'Welcome to my website! Click here for a tour!';
+}
