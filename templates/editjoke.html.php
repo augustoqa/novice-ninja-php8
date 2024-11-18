756052582b59f4dbc2e1b78a815e369de764548e
Augusto Quisquinay <augustoacual@gmail.com>
2024-11-18 16:18:05 -0600

    Add feature for update categories when update a joke

--- templates/editjoke.html.php
+++ templates/editjoke.html.php
@@ -10,7 +10,11 @@
 
 	<p>Select categories for this joke:</p>
 	<?php foreach ($categories as $category): ?>
-	<input type="checkbox" name="category[]" value="<?= $category->id ?>" />
+		<?php if ($joke && $joke->hasCategory($category->id)): ?>	
+		<input type="checkbox" name="category[]" value="<?= $category->id ?>" checked />
+		<?php else: ?>
+		<input type="checkbox" name="category[]" value="<?= $category->id ?>" />
+		<?php endif ?>
 	<label><?= $category->name ?></label>
 	<?php endforeach ?>
 
